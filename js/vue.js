2380d94a591501b4ff1ffbbf9d5b71b0ab8de5b3
jefferson lucas <jefferson.lucas114@gmail.com>
2018-10-23 13:55:44 -0300

    testando vue.js

--- js/vue.js
+++ js/vue.js
@@ -1,20 +1,52 @@
 
-
 var app = new Vue({
   el: '#app' ,
   data: {
-    message: 0,
-    message1: 0,
-    
+    nome: "",
+    email: "",
+    numero: "",
+    idade: "",
+    resposta: "",
+    qualidades: [],
+    usuarios: [
+      {nome: 'israel'},
+      {nome: 'jeff'},
+      {nome: 'caio'}
+    ]
   },
-  computed: {
 
-    multiplicacao: function () {
-
-     return  this.message * this.message1;
-
+  filtres: {
+    toUpperCase(str) {
+      return str.toUpperCase();
     }
+  },
+
+  methods: {
+    enviar() {
+      alert('enviado');
+    },
   }
+  });
 
-})
 
+    
+//   },
+//   computed: {
+
+//     multiplicacao: function () {
+    
+//      return  this.message * this.message1;
+
+//     }
+//   }
+
+// })
+
+// new Vue ({ 
+//   el: '#app' , 
+//   métodos: { 
+//     diga: function ( message ) { 
+//       alert (message) 
+//     } 
+//   } 
+// })
